f13207a78cbd7ad305c50b4dae7f300bf98de0b9
itkebbai <MuYunShenyyds@outlook.com>
2022-03-13 14:36:41 +0800

    master commit1

--- src/test/java/com/itkebbai/demo.java
+++ src/test/java/com/itkebbai/demo.java
@@ -5,5 +5,6 @@ public class demo {
         System.out.println("nihao");
         System.out.println("nihao1");
         System.out.println("nihao2");
+        System.out.println("master test");
     }
 }
