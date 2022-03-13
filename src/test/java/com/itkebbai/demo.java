bf0463a65c1db77eab3cddd1cfe0834d9f314c9a
itkebbai <MuYunShenyyds@outlook.com>
2022-03-13 14:19:29 +0800

    hof-fix commit

--- src/test/java/com/itkebbai/demo.java
+++ src/test/java/com/itkebbai/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("nihao");
         System.out.println("nihao1");
+        System.out.println("nihao2");
     }
 }
