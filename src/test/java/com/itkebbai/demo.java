244903d75ce2bc393a62e239c9688642d97bd6a5
itkebbai <MuYunShenyyds@outlook.com>
2022-03-19 12:11:45 +0800

    push commit1

--- src/test/java/com/itkebbai/demo.java
+++ src/test/java/com/itkebbai/demo.java
@@ -7,5 +7,6 @@ public class demo {
         System.out.println("nihao2");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
